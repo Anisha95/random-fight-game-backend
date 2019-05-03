ba2975bfbde28b05a9ca0e97f021ca8300c83742
Anisha <pal.anisha26@gmail.com>
2019-05-03 11:44:07 +0530

    api will be providing response when there is only one player left inactive!

--- routes/index.js
+++ routes/index.js
@@ -37,15 +37,32 @@ router.post('/hero', function (req, res) {
 
 router.get('/fight', function(req, res) {
      Players.aggregate([ {$match : {active : true}}, {$sample : {size : 2} }]).then(function (players) {
+
         var b  = (players);
+        console.log(b);
         var c = floor(random());
+         console.log('Hello 1');
         var d = 1-c;
         var x = b[c];
+        console.log('Hello 2');
         var y = b[d];
+        console.log('Hello 3');
         x.active = false;
-        Players.findByIdAndUpdate({"_id" : x._id}, { $set: {"active" : false}},  function(err, user){});
+        //console.log(y);
+        if(y == undefined){
+            res.send('Only 1 Player left! Please create a new player..');
+            res.status(401);
+            return false;
+        }
+        Players.findByIdAndUpdate({"_id" : x._id}, { $set: {"active" : false}},  function(err, user){
+            // if(err){
+            //     res.send('You have only 1 player left!');
+            //     throw err;
+            // }
+        });
         Players.findByIdAndUpdate({"_id" : y._id}, { $inc: { score: 30} },  function(err, user){});
         var fight = new Fights();
+
         fight.player_1_id = x._id;
         fight.player_1_name = x.name;
         fight.player_2_id = y._id;
